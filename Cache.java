26ec60593465237da31c097019d7a6d88494e05a
André Peil <andreguipeil@gmail.com>
2013-01-23 00:50:50 -0200

    Versão 0.9

--- Cache.java
+++ Cache.java
@@ -1,10 +1,12 @@
+
 import java.util.Random;
 import java.util.LinkedList;
+
 public class Cache {
 
-    LinkedList<Integer> historicoCache = new LinkedList<Integer>();
     Celula celulas[];
     Cache nivelInferior;
+    private LinkedList<Integer> historicoCache = new LinkedList<Integer>();
     private int mBits_offset;
     private int mBits_indice;
     private int mBits_tag;
@@ -19,234 +21,242 @@ public class Cache {
     private int num_misses;
     private String politicaAplicada;
     private Random gerar;
+
     /**
-     *  Construtor de uma cache
+     * Construtor de uma cache
+     *
      * @param int nsets númerode celulas de memória
      * @param int bsize tamanho de cada célula
      * @param assoc nível de associatividade
      * @param politica política usada: {'random','LRU'}
      */
-    public Cache (int nsets, int bsize, int assoc, String politica){
-        
-        try{
-            
+    public Cache(int nsets, int bsize, int assoc, String politica) {
+
+        try {
+
             celulas = new Celula[nsets];
-            
-            for(int a=0; a < nsets; a++){
+
+            for (int a = 0; a < nsets; a++) {
                 celulas[a] = new Celula(assoc);
             }
-            
-        }catch(Exception e){
-            
-            System.out.println("Error 5.100! Leia o 'README'"+e.toString());
-            
+
+        } catch (Exception e) {
+
+            System.out.println("Error 5.100! Leia o 'README'" + e.toString());
+
         }
         num_enderecos_processados = num_hits = num_misses = 0;
-        mBits_offset = (int)(Math.log(bsize) / Math.log(2));
-        mBits_indice = (int)(Math.log(nsets) / Math.log(2));
-        mBits_tag = 32 -  mBits_offset - mBits_indice;
+        mBits_offset = (int) (Math.log(bsize) / Math.log(2));
+        mBits_indice = (int) (Math.log(nsets) / Math.log(2));
+        mBits_tag = 32 - mBits_offset - mBits_indice;
         politicaAplicada = politica.toLowerCase();
         gerar = new Random();
-       // System.out.println("mBits_offset: "+mBits_offset+" mBits_indice: "+mBits_indice+" mBits_tag: "+mBits_tag+" Política Usada:"+politicaAplicada);
+        numMissCapacidade = 0;
+        // numMissConflito = 0; -- por enquanto não usa
+        numMissCompulsorio = 0;
+        // System.out.println("mBits_offset: "+mBits_offset+" mBits_indice: "+mBits_indice+" mBits_tag: "+mBits_tag+" Política Usada:"+politicaAplicada);
     }
-    
+
     /**
      * Construtor de uma cache
+     *
      * @param int nsets númerode celulas de memória
      * @param int bsize tamanho de cada célula
      * @param assoc nível de associatividade
      * @param politica política usada: {'random','LRU'}
      * @param Cache l2 cache atuante como secundária
      */
-    public Cache (int nsets, int bsize, int assoc, String politica, Cache l2){
-        this(nsets,bsize,assoc,politica);
+    public Cache(int nsets, int bsize, int assoc, String politica, Cache l2) {
+        this(nsets, bsize, assoc, politica);
         this.nivelInferior = l2;
     }
-    
+
     /**
      * Retorna o nómerode bits usados como offiset
+     *
      * @return
      */
-    public int getmBits_offset(){
+    public int getmBits_offset() {
         return mBits_offset;
     }
-    
+
     /**
-     * Retorna o número de bits usados
-     *  para o índice
-     * @return 
+     * Retorna o número de bits usados para o índice
+     *
+     * @return
      */
-    public int getmBits_indice(){
+    public int getmBits_indice() {
         return mBits_indice;
     }
-    
+
     /**
-     * Retorna o número de bits usados
-     *  para serem tag
-     * @return 
+     * Retorna o número de bits usados para serem tag
+     *
+     * @return
      */
-    public int getmBits_tag(){
+    public int getmBits_tag() {
         return mBits_tag;
     }
-    
+
     /**
-     * Método que retorna o resultado da procura do endereço
-     *  boolean{true - Hit | false - Miss}
+     * Método que retorna o resultado da procura do endereço boolean{true - Hit
+     * | false - Miss}
+     *
      * @param enderecoRecebido
      * @return boolean resultadoDaProcura
      */
-    public boolean manipula(int enderecoRecebido){
-        
+    public boolean manipula(int enderecoRecebido) {
+
         historicoCache.add(enderecoRecebido);
 
         this.prepararEndereco(enderecoRecebido);
         this.num_enderecos_processados++;
         boolean resultadoDaProcura = acharEnderecoNaCache();
-        
-        
+
+
         /**
-         * Se o valor não está presente, então aplica a política
-         *  adeguada
+         * Se o valor não está presente, então aplica a política adequada
          */
-        if(!resultadoDaProcura){
-            if( "random".equals(politicaAplicada) ){
-                
+        if (!resultadoDaProcura) {
+            if ("random".equals(politicaAplicada)) {
+
                 this.politicaRandomica();
-                
-            }else{
-            if( "lru".equals(politicaAplicada) ){
-                
+
+            } else {
+                if ("lru".equals(politicaAplicada)) {
+
                     this.politicaLRU();
-                    
-                }else{
-                
+
+                } else {
+
                     this.politicaRandomica();
-                    
+
                 }
             }
         }
-        
-        if(resultadoDaProcura){
-            this.num_hits+=1;
-        }else{
-            this.num_misses+=1;
+
+        if (resultadoDaProcura) {
+            this.num_hits += 1;
+        } else {
+            this.num_misses += 1;
         }
-        
-        
-       return resultadoDaProcura;
-        
+
+
+        return resultadoDaProcura;
+
     }
-    
+
     /**
-     * Método que procura nas celula (linha da cache) igual a this.indice
-     *  se o valor da this.tag está presente
-     * @return 
+     * Método que procura nas celula (linha da cache) igual a this.indice se o
+     * valor da this.tag está presente
+     *
+     * @return
      */
-    private boolean acharEnderecoNaCache(){
+    private boolean acharEnderecoNaCache() {
         boolean achouEndereco = false;
-        
-        for(int n_assoc=0; n_assoc < celulas[0].getAssociatividadeLength(); n_assoc++ ){
-            if( celulas[this.indice].getValidade(n_assoc)){
-                if(celulas[this.indice].getTag(n_assoc) == this.tag){
+
+        for (int n_assoc = 0; n_assoc < celulas[0].getAssociatividadeLength(); n_assoc++) {
+            if (celulas[this.indice].getValidade(n_assoc)) {
+                if (celulas[this.indice].getTag(n_assoc) == this.tag) {
                     achouEndereco = true;
                     break;
                 }
             }
         }
-        
-        if(achouEndereco){
+
+        if (achouEndereco) {
             return achouEndereco;
-        }else{
-            if(this.nivelInferior != null){
+        } else {
+            if (this.nivelInferior != null) {
                 achouEndereco = this.nivelInferior.manipula(endereco);
             }
-            
+
         }
         return achouEndereco;
     }
-    
+
     /**
      * Método que calcula o indice e tag para o endereço manipulado
-     * @param int enderecoRecebido 
+     *
+     * @param int enderecoRecebido
      */
-   private void prepararEndereco(int enderecoRecebido){
-       try{
-            if (enderecoRecebido < 0){
-                this.endereco = ~enderecoRecebido;
-                this.endereco-=this.endereco;
-                System.out.println(this.endereco);
-            } else{
-                this.endereco = enderecoRecebido;
-            }
-            
-            Double aa = Math.pow(2,this.getmBits_indice())-1;
-            this.indice = ( endereco >> this.getmBits_offset());
-            this.indice = ( this.indice & aa.byteValue() );
-            this.tag = ( this.endereco >> (byte)(this.mBits_offset + this.mBits_indice) );
-            System.out.println("Endereço:"+Integer.toBinaryString(enderecoRecebido) +"\n  Indice:"+Integer.toBinaryString(indice) +"\n  Tag:"+Integer.toBinaryString(this.tag) +"\n");
-        }catch(Exception e){
+    private void prepararEndereco(int enderecoRecebido) {
+        try {
+            this.historicoCache.add(enderecoRecebido);
+            this.endereco = enderecoRecebido;
+            Double aa = Math.pow(2, this.getmBits_indice()) - 1;
+            this.indice = (endereco >> this.getmBits_offset());
+            this.indice = (this.indice & aa.byteValue());
+            this.tag = (this.endereco >> (byte) (this.mBits_offset + this.mBits_indice));
+            System.out.println("Numero: " + enderecoRecebido);
+            System.out.println("Endereço:" + Integer.toBinaryString(enderecoRecebido) + "\n  Indice:" + Integer.toBinaryString(indice) + "\n  Tag:" + Integer.toBinaryString(this.tag) + "\n");
+        } catch (Exception e) {
             System.out.println("Erro 5.101! Leia o 'README'");
         }
-        
-        
-   }
-   
-     /**
-     * Aplica a politica Radomica da inserção na linha manipulada ( this.indice). 
-     * a celula (linha da cache) manipulada receberá randomicamente em um
-     *  das suas posições associativas
+    }
+
+    /**
+     * Aplica a politica Radomica da inserção na linha manipulada (
+     * this.indice). a celula (linha da cache) manipulada receberá
+     * randomicamente em um das suas posições associativas
      */
-    private void politicaRandomica(){
+    private void politicaRandomica() {
         int num_random_assoc = celulas[0].getAssociatividadeLength();
-        if( num_random_assoc != 1){
-            num_random_assoc = gerar.nextInt( celulas[0].getAssociatividadeLength()-1 );
-        }else{
+        if (num_random_assoc != 1) {
+            num_random_assoc = gerar.nextInt(celulas[0].getAssociatividadeLength() - 1);
+        } else {
             num_random_assoc = 0;
         }
-        
-        try{
+
+        try {
             System.out.println(this.celulas.length);
             /*for(int i = 0; i < this.celulas.length; i++){
-                System.out.println(i +" "+celulas[i].getTag(0)+" "+celulas[i].getValidade(0));
+             System.out.println(i +" "+celulas[i].getTag(0)+" "+celulas[i].getValidade(0));
             
-            }*/
-            
-            
-            //if (celulas[this.indice])
-            
+             }*/
+
+
+            if (!celulas[this.indice].getValidade(num_random_assoc)) {
+                numMissCompulsorio++;
+            } else {
+                if (historicoCache.contains(this.endereco)) {
+                    numMissCapacidade++;
+                } else {
+                    numMissConflito++;
+                }
+            }
+
             celulas[this.indice].setValido(num_random_assoc);
-            celulas[this.indice].setTag(num_random_assoc, this.tag);            
-        }catch(Exception e){
+            celulas[this.indice].setTag(num_random_assoc, this.tag);
+        } catch (Exception e) {
             System.out.println("Erro 5.102! Leia o 'README'");
         }
     }
-    
-    
-    /**
-     * Aplica a politica LRU da inserção na linha manipulada ( this.indice). 
-     *  a celula (linha da cache) manipulada receberá o dado na mais antiga das inserções
-     */
-   private void politicaLRU(){
-       System.out.println("\nMiss\nPolitica não implementada");
 
-   }
-   
-   public String gerarRelatorio(){
-       String retorno = "";
-        if(this.nivelInferior != null){
-            retorno = "\nL2\n"+this.nivelInferior.gerarRelatorio();
+    /**
+     * Aplica a politica LRU da inserção na linha manipulada ( this.indice). a
+     * celula (linha da cache) manipulada receberá o dado na mais antiga das
+     * inserções
+     */
+    private void politicaLRU() {
+        System.out.println("\nMiss\nPolitica não implementada");
+
+    }
+
+    public String gerarRelatorio() {
+        String retorno = "";
+        if (this.nivelInferior != null) {
+            retorno = "\nL2\n" + this.nivelInferior.gerarRelatorio();
         }
-        
+
         double missRatio, hitRatio;
-        try{
+        try {
             missRatio = (num_misses * 100) / this.num_enderecos_processados;
-            hitRatio  = (num_hits * 100) / this.num_enderecos_processados;
-       }catch(Exception e){
+            hitRatio = (num_hits * 100) / this.num_enderecos_processados;
+        } catch (Exception e) {
             missRatio = 0.0;
-            hitRatio  = 0.0;
-       }
-       return String.format("Total Endereços: %d\nTotal Hits: %d\nTotal Misses: %d\nHit Ratio: %.1f \nMiss Ratio: %.1f\n============"+retorno, this.num_enderecos_processados, this.num_hits, this.num_misses, hitRatio, missRatio);
-   }
-   
-   
+            hitRatio = 0.0;
+        }
+        return String.format("Total Endereços: %d\nTotal Hits: %d\nTotal Misses: %d\n - Miss Compulsório: %d\n - Miss Capacidade ou Conflito: %d \nHit Ratio: %.1f \nMiss Ratio: %.1f\n============" + retorno, this.num_enderecos_processados, this.num_hits, this.num_misses, this.numMissCompulsorio, this.numMissCapacidade, hitRatio, missRatio);
+    }
 }
--- Simulador.java
+++ Simulador.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,16 +8,17 @@ import java.util.Scanner;
 public class Simulador {
 
     Cache l1, l2;
+
     public static void main(String[] args) {
 
-        if(args.length == 3){
+        if (args.length == 3) {
 
             try {
                 Simulador main = new Simulador();
-                main.iniciar(args[0] , args[1], args[2]);
+                main.iniciar(args[0], args[1], args[2]);
                 main.finalizar();
-            
-            } catch (Exception e){
+
+            } catch (Exception e) {
                 System.out.println("Erro 5.000! Leia o 'README'");
                 e.printStackTrace();
             }
@@ -28,194 +30,183 @@ public class Simulador {
 
     }
 
-    public void iniciar(String cache1, String cache2, String nomeArquivoBin){
-        
-        Scanner entrada = new Scanner(System.in);
-        
+    public void iniciar(String cache1, String cache2, String nomeArquivoBin) {
+
+        //Scanner entrada = new Scanner(System.in);  // somente para teste com paradas
+
         int nSetsCache1, bSizeCache1, associacaoCache1;
         int nSetsCache2, bSizeCache2, associacaoCache2;
         String[] configCache1, configCache2;
-        
+
         nSetsCache1 = nSetsCache2 = 1;
-        bSizeCache1 = bSizeCache2 =32;
+        bSizeCache1 = bSizeCache2 = 32;
         associacaoCache1 = associacaoCache2 = 1024;
-        
-        
+
+
         /**
          * Faz a conversão de String para int
          */
-        try{
-            
+        try {
+
             /**
-             * Recebe os parametros juntos na string cache1 = "<nsets_L1>:<bsize_L1>:<assoc_L1>"
+             * Recebe os parametros juntos na string cache1 =
+             * "<nsets_L1>:<bsize_L1>:<assoc_L1>"
              */
             configCache1 = cache1.split(":");
             configCache2 = cache2.split(":");
-            
+
             nSetsCache1 = Integer.parseInt(configCache1[0]);
-            if(nSetsCache1 < 1){
-                nSetsCache1 = 1;  
+            if (nSetsCache1 < 1) {
+                nSetsCache1 = 1;
             }
-                
+
             bSizeCache1 = Integer.parseInt(configCache1[1]);
-            if(bSizeCache1 < 1){
-                bSizeCache1 = 4;  
+            if (bSizeCache1 < 1) {
+                bSizeCache1 = 4;
             }
-                
+
             associacaoCache1 = Integer.parseInt(configCache1[2]);
-            if(associacaoCache1 < 1 || associacaoCache1 > nSetsCache1){
-                associacaoCache1 = 1024;  
+            if (associacaoCache1 < 1 || associacaoCache1 > nSetsCache1) {
+                associacaoCache1 = 1024;
             }
-            
-            
+
+
             nSetsCache2 = Integer.parseInt(configCache2[0]);
-            if(nSetsCache2 < 1){
-                nSetsCache2 = 1;  
+            if (nSetsCache2 < 1) {
+                nSetsCache2 = 1;
             }
-            
+
             bSizeCache2 = Integer.parseInt(configCache2[1]);
-            if(bSizeCache2 < 1){
-                bSizeCache2 = 4;  
+            if (bSizeCache2 < 1) {
+                bSizeCache2 = 4;
             }
-            
+
             associacaoCache2 = Integer.parseInt(configCache2[2]);
-            if(associacaoCache2 < 1 || associacaoCache2 > nSetsCache2){
-                associacaoCache2 = 1024;  
+            if (associacaoCache2 < 1 || associacaoCache2 > nSetsCache2) {
+                associacaoCache2 = 1024;
             }
             
-            
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error 5.002! Leia o 'README'");
         }
-        
+
         /**
          * Cria as caches
          */
-        
-        l2 = new Cache(nSetsCache2,bSizeCache2,associacaoCache2,"random");
-        l1 = new Cache(nSetsCache1,bSizeCache1,associacaoCache1, "random", l2);
-        
+        l2 = new Cache(nSetsCache2, bSizeCache2, associacaoCache2, "random");
+        l1 = new Cache(nSetsCache1, bSizeCache1, associacaoCache1, "random", l2);
+
         int enderecoEmInteger;
         boolean missHit;
         ArquivoBinario arquivo = new ArquivoBinario(nomeArquivoBin);
-        while(arquivo.temProximaPalavra32()){
-            
+        while (arquivo.temProximaPalavra32()) {
+
             enderecoEmInteger = arquivo.getProximaPalavra32Bits();
 
-            
-           // entrada.nextLine();
+
+         // entrada.nextLine(); // ==> utilizado para parar o programa(apenas para teste)
             missHit = l1.manipula(enderecoEmInteger);
-            if(missHit){
-                System.out.println("Hit!");
-            }else{
-                System.out.println("Miss!");
-            }
         }
-        //for (int r = 0; r < l1.historicoCache.size(); r++){
-        //    System.out.println(l1.historicoCache.get(r));
-        //}
-        
-
-    }
-    public void finalizar(){
-        System.out.println("\nRelatório Final:\nL1\n"+this.l1.gerarRelatorio());
     }
 
+    public void finalizar() {
+        System.out.println("\nRelatório Final:\nL1\n" + this.l1.gerarRelatorio());
+    }
 }
 
 /**
  * Classe que manipula o arquivo
+ *
  * @author glaucoroberto
  */
-class ArquivoBinario{
+class ArquivoBinario {
+
     File arquivo;
     InputStream arquivoStream;
     int conteudoLido;
     boolean terminouArquivo = false;
-    
-    private ArquivoBinario(){
-        
+
+    private ArquivoBinario() {
     }
-    
+
     /**
      * Abre o arquivo binário enviado como paramentro
-     * 
-     * @param nomeArquivo 
+     *
+     * @param nomeArquivo
      */
-    public ArquivoBinario(String nomeArquivo){
+    public ArquivoBinario(String nomeArquivo) {
         arquivo = new File(nomeArquivo);
 
-        try{
+        try {
             arquivoStream = new FileInputStream(arquivo);
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.printf("Error 5.900! Leia o 'README'");
         }
     }
-    
+
     /**
-     * Verifica se possui uma próxima palavra de 32bits
-     *  ou equivalente a um int
-     * @return true - se possúi | false - se não possúi
+     * Verifica se possui uma próxima palavra de 32bits ou equivalente a um int
+     *
+     * @return true - se possui | false - se não possui
      */
-    public boolean temProximaPalavra32(){
+    public boolean temProximaPalavra32() {
         int temp = 0;
         conteudoLido = 0;
-        if(terminouArquivo){
+        if (terminouArquivo) {
             return false;
         }
-        
-        // mexer nisso aqui
-        // shiftar 8bits a cada passada do for
-        
-        try{
+
+        try {
             boolean voltaOQue = false;
-            
-            for(int a = 0; a < 4; a++){
-                if( (temp = (int) arquivoStream.read() ) != -1 ){
-                   // System.out.println(temp);
-                    
+
+            for (int a = 0; a < 4; a++) {
+                if ((temp = (int) arquivoStream.read()) != -1) {
+                    // System.out.println(temp);
+
                     if (a != 3)//{
+                    {
                         conteudoLido = conteudoLido | temp;
-                        conteudoLido = (conteudoLido << 4);
+                    }
+                    conteudoLido = (conteudoLido << 4);
                     /*} else {
-                        temp = ~ temp;
-                        conteudoLido = conteudoLido | temp;
-                    }*/
+                     temp = ~ temp;
+                     conteudoLido = conteudoLido | temp;
+                     }*/
                     voltaOQue = true;
-                }else{
+                } else {
                     terminouArquivo = true;
                     voltaOQue = false;
                 }
             }
-            conteudoLido = conteudoLido+temp;
-           // System.out.println("agora:"+Integer.toBinaryString(conteudoLido));
-            
-            
+            conteudoLido = conteudoLido + temp;
             return voltaOQue;
-            
-        }catch(IOException ex){
-            System.out.println("Error 5.901! Leia o 'README'"+"/nVeja o erro: "+ex.getMessage() );
+
+        } catch (IOException ex) {
+            System.out.println("Error 5.901! Leia o 'README'" + "/nVeja o erro: " + ex.getMessage());
             return false;
         }
     }
-    
+
     /**
      * Retorna um integer com os 32bits
+     *
      * @return int
      */
-    public int getProximaPalavra32Bits(){
-        if(terminouArquivo){
+    public int getProximaPalavra32Bits() {
+        if (terminouArquivo) {
             return -1;
         }
         return conteudoLido;
     }
-    
+
     /**
-     * Retorna os 32bits lidos na forma de String contendo 32bits em forma de caract.
+     * Retorna os 32bits lidos na forma de String contendo 32bits em forma de
+     * caract.
+     *
      * @return String
      */
-    public String getProximaPalavra32String(){
-        if(terminouArquivo){
+    public String getProximaPalavra32String() {
+        if (terminouArquivo) {
             return null;
         }
         return Integer.toBinaryString(conteudoLido);
